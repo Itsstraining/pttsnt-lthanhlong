d7174163dba02deaec8f11ff5f3666f7dcaa426e
Le Thanh Long <long.lt1212@sinhvien.hoasen.edu.vn>
2021-01-29 10:22:01 +0700

    Lan 1

--- program.js
+++ program.js
@@ -3,6 +3,20 @@
 function main(input) {
   // Your code goes here
   // Using console.log to answer the question
+  function isprime(n)
+let j =Math.sqrt(n);
+  for (i = 2, i <= j, i++)
+  {
+    if (n % i == 0) 
+    {
+      return false;
+    }
+    
+      return true;
+    
+  }
 }
+
+
 
 module.exports = main;
